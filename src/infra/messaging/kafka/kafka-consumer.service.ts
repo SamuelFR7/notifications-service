a65252551404257cf76403c036590b90b1585b2f
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-21 10:59:38 -0300

    feat: kafka implementation

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,27 @@
+import { ServerKafka } from '@nestjs/microservices';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+      client: {
+        clientId: process.env.KAFKA_CLIENTID,
+        brokers: [`${process.env.KAFKA_BROKER}`],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username: `${process.env.KAFKA_USERNAME}`,
+          password: `${process.env.KAFKA_PASSWORD}`,
+        },
+        ssl: true,
+      },
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
